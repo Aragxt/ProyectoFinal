25ab69168b8796917a2573e30ba559cefe2dd8f2
Your Name <hriosi@unal.edu.co>
2022-06-29 02:14:18 -0500

    Actualizo HR

---
+++ Laravel/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+
+@extends('layouts.app')
+
+@section('content')
+
+    <body>
+        <a type="submit" class="btn btn-primary" href="//localhost/ProyectoFinal/index.html">
+            Principal
+        </a>
+    </body>
+</html>
